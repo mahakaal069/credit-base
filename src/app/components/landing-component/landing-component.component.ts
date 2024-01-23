36ae846b33bef6541190d22090809ddccffa3379
mahakaal069 <Kumar.tiwari211@gmail.com>
2024-01-23 10:45:27 +0530

    created initial components and sub-folders

---
+++ src/app/components/landing-component/landing-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing-component.component.html',
+  styleUrl: './landing-component.component.sass'
+})
+export class LandingComponentComponent {
+
+}
