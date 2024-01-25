9c16fdf25e3eb9fa21debcd83286ed84633bd325
Brajesh.Tiwari <Brajesh.Tiwari@equitable.com>
2024-01-25 06:17:19 -0500

    Initialized the app with angular material dependency

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,20 +5,45 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { RecaptchaVerifier, getAuth, provideAuth } from '@angular/fire/auth';
-import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import {
+    initializeAppCheck,
+    ReCaptchaEnterpriseProvider,
+    provideAppCheck,
+} from '@angular/fire/app-check';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { firebaseApp, firebaseConfig } from './config/firebase-app-config';
 import { environment } from '../environments/environment';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 const reCaptchaEnterpriseSiteKey: string = environment['reCaptchaEnterpriseSiteKey'];
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp(firebaseConfig))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideAppCheck(() => {
-        // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=credit-base-65f11
-        const provider = new ReCaptchaEnterpriseProvider(reCaptchaEnterpriseSiteKey);
-        return initializeAppCheck(firebaseApp, { provider, isTokenAutoRefreshEnabled: true });
-    })), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase())), importProvidersFrom(provideMessaging(() => getMessaging())), importProvidersFrom(provideStorage(() => getStorage()))]
+    providers: [
+        provideRouter(routes),
+        provideClientHydration(),
+        importProvidersFrom(
+            provideFirebaseApp(() => initializeApp(firebaseConfig))
+        ),
+        importProvidersFrom(provideAuth(() => getAuth())),
+        importProvidersFrom(
+            provideAppCheck(() => {
+                // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=credit-base-65f11
+                const provider = new ReCaptchaEnterpriseProvider(
+                    reCaptchaEnterpriseSiteKey
+                );
+                return initializeAppCheck(firebaseApp, {
+                    provider,
+                    isTokenAutoRefreshEnabled: true,
+                });
+            })
+        ),
+        importProvidersFrom(provideFirestore(() => getFirestore())),
+        importProvidersFrom(provideDatabase(() => getDatabase())),
+        importProvidersFrom(provideMessaging(() => getMessaging())),
+        importProvidersFrom(provideStorage(() => getStorage())),
+        provideAnimations(),
+    ],
 };
